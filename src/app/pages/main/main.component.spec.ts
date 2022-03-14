db06e883234f6af92bd7071569dcb88bb75b4552
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2022-03-14 06:45:13 -0300

    Implements unit testing for main component

--- src/app/pages/main/main.component.spec.ts
+++ src/app/pages/main/main.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
+import { ChessBoardMovement } from 'src/app/model/movement';
+import { FrameCommunicationService } from 'src/app/services/frame-communication.service';
 import { WindowMock } from 'src/teste-utils';
 
 import { MainComponent } from './main.component';
@@ -8,11 +11,23 @@ describe('MainComponent', () => {
   let fixture: ComponentFixture<MainComponent>;
   let windowMock: Window;
 
+  const frameCommunicationService = {
+    onCheckMate: () => new Subject<ChessBoardMovement>().asObservable(),
+    restorePrevious: () => {},
+    reset: () => {},
+  };
+
   beforeEach(async () => {
     windowMock = WindowMock as any;
     await TestBed.configureTestingModule({
       declarations: [MainComponent],
-      providers: [{ provide: 'Window', useFactory: () => windowMock }],
+      providers: [
+        {
+          provide: FrameCommunicationService,
+          useValue: frameCommunicationService,
+        },
+        { provide: 'Window', useFactory: () => windowMock },
+      ],
     }).compileComponents();
   });
 
@@ -24,5 +39,55 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should subscribe for "onCheckMate" event', () => {
+    spyOn(frameCommunicationService, 'onCheckMate').and.callThrough();
+    component.ngOnInit();
+    expect(frameCommunicationService.onCheckMate).toHaveBeenCalled();
+  });
+
+  it('should check for restore previous state on afterViewChecked method', () => {
+    spyOn(frameCommunicationService, 'restorePrevious').and.stub();
+    component.ngAfterViewChecked();
+    expect(frameCommunicationService.restorePrevious).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call reset service method on reset', () => {
+    spyOn(frameCommunicationService, 'reset').and.stub();
+    component.reset();
+    expect(frameCommunicationService.reset).toHaveBeenCalledTimes(1);
+  });
+
+  it('should display an alert on checkMate event', () => {
+    const subject = new Subject<ChessBoardMovement>();
+    spyOn(frameCommunicationService, 'onCheckMate').and.returnValue(
+      subject.asObservable()
+    );
+    spyOn(window, 'alert').and.stub();
+
+    component.ngOnInit();
+    subject.next({ color: 'black' } as ChessBoardMovement);
+
+    expect(frameCommunicationService.onCheckMate).toHaveBeenCalled();
+    expect(window.alert).toHaveBeenCalledOnceWith(
+      'The black pieces have won the match!'
+    );
+  });
+
+  it('should call reset service method on checkMate event', () => {
+    const subject = new Subject<ChessBoardMovement>();
+
+    spyOn(window, 'alert').and.stub();
+    spyOn(frameCommunicationService, 'reset').and.stub();
+    spyOn(frameCommunicationService, 'onCheckMate').and.returnValue(
+      subject.asObservable()
+    );
+
+    component.ngOnInit();
+    subject.next({ color: 'black' } as ChessBoardMovement);
+
+    expect(frameCommunicationService.onCheckMate).toHaveBeenCalled();
+    expect(frameCommunicationService.reset).toHaveBeenCalledTimes(1);
   });
 });
